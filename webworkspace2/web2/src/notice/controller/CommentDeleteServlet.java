c27f69c56185c10095c58b4f58b21cd3c2f8b91c
minimelodi <minimelodi@naver.com>
2018-05-30 18:23:02 +0900

    20180530

---
+++ webworkspace2/web2/src/notice/controller/CommentDeleteServlet.java
@@ -0,0 +1,49 @@
+package notice.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import notice.model.service.NoticeService;
+
+/**
+ * Servlet implementation class CommentDeleteServlet
+ */
+@WebServlet(name = "CommentDelete", urlPatterns = { "/commentDelete" })
+public class CommentDeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CommentDeleteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int commentNo = Integer.parseInt(request.getParameter("commentNo"));
+		int noticeNo = Integer.parseInt(request.getParameter("noticeNo"));
+		int result = new NoticeService().deleteComment(commentNo);
+		if(result>0) {
+			response.sendRedirect("noticeSelect?noticeNo="+noticeNo);
+		}else {
+			response.sendRedirect("views/notice/Error.html");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
